b4948aa3476eed2bc84aec3c94476fc0a398624c
Johnny Chang <johnny@johnnychang.com>
2016-02-04 19:01:40 -0600

    Added carousel feature to title-section, changed title section to an id instead of a class.

--- source_js/script.js
+++ source_js/script.js
@@ -1,16 +1,67 @@
-var divs = document.getElementsByTagName('div');
-for(var i=0; i<divs.length; i++) {
-  divs[i].addEventListener("click", highlightThis);
-  /*
-  divs[i].addEventListener("click", highlightThis, true);
-  divs[i].addEventListener("click", highlightThis, false);*/
-}
+$(document).ready(function() {
+	/*
+	 * Carousel.
+	 * Functions by animating a change of background images for the title section.
+	 */
 
-function highlightThis(event) {
-    //event.stopPropagation();
+	var MEDIA_DIR = "media/";
 
-    var backgroundColor = this.style.backgroundColor;
-    this.style.backgroundColor='yellow';
-    alert(this.className);
-    this.style.backgroundColor=backgroundColor;
-}
+	var carousel_index = 0;
+
+	// Add new images here.
+	var images = [
+		"bus_vs_village_far.jpg",
+		"ox_cart.jpg",
+		"boy_running.jpg",
+		"ride_with_us.jpg"
+	];
+
+	$("#title-right").click(moveRight);
+	$("#title-left").click(moveLeft);
+
+	function moveRight() {
+		console.log("moveRight clicked.");
+		carousel_index = incrementCarousel(carousel_index, images.length);
+		changeBackgroundImage(carousel_index);
+	}
+
+	function moveLeft() {
+		console.log("moveLeft clicked.");
+		carousel_index = decrementCarousel(carousel_index, images.length);
+		changeBackgroundImage(carousel_index);
+	}
+
+	function incrementCarousel(idx, len) {
+		if (idx == len - 1) {
+			idx = 0;
+		} else {
+			idx++;
+		}
+
+		return idx;
+	}
+
+	function decrementCarousel(idx, len) {
+		if (idx == 0) {
+			idx = len - 1;
+		} else {
+			idx--;
+		}
+
+		return idx;
+	}
+
+	function changeBackgroundImage(idx) {
+		var image_url = "url('" + MEDIA_DIR + images[idx] + "')";
+		var background_size = "cover";
+
+		console.log(image_url);
+		$("#title-section").css(
+			{"background-image": "linear-gradient(rgba(0, 21, 42, 0.50), rgba(0, 21, 42, 0.50)), " + image_url}, 
+			{"background-size": background_size},
+			{"webkit-background-size": background_size},
+    		{"-moz-background-size": background_size},
+		    {"-o-background-size": background_size},
+		    {"background-position": "0% 25%"});
+	}
+});
